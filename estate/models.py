38d906526ee68925227095c173aa9f3390094395
0724654276 <you@example.com>
2022-01-10 09:53:10 +0300

    create model for neighborhood

--- estate/models.py
+++ estate/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Neighborhood(models.Model):
+    neighborhood_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100,null=True)
+    occupant_count = models.IntegerField(null=True)
+    
+    
