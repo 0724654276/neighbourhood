1f4ee3b9d9d5c27def43945e4507e0b5c0386733
0724654276 <you@example.com>
2023-01-12 19:16:52 +0300

    change debug option

--- hoodproject/settings.py
+++ hoodproject/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c8)i=y(*m)wp%crnffb+=x%75+lq-%10uhk_3np@@dozc4+g)i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
